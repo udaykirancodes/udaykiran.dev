2ece48a6634f225e2abeda0904192c92ee612aa2
UdayKiran <uday@hexacluster.ai>
2025-08-07 11:37:32 +0530

    add tech-stack svg files

--- src/components/profile/tech-stack.tsx
+++ src/components/profile/tech-stack.tsx
@@ -1,20 +1,25 @@
-import { TECH_STACK } from "@/data/user"
+"use client"
+
+import { TECH_STACK } from "@/data/tech-stack"
+import { useTheme } from "next-themes"
 import Image from "next/image"
 import { Button } from "../ui/button"
 import { HeaderTitle } from "./header-title"
 
 export const TechStack: React.FC = () => {
+  const { theme } = useTheme()
   return (
     <>
       <section className="w-full">
         <HeaderTitle title="Tech Stack" />
         <div className="grid w-full grid-cols-4 gap-2 p-2 lg:grid-cols-6">
           {TECH_STACK.map((item) => {
+            const url = `/assets/tech-stack/${item.key}${item.theme ? (theme === "dark" ? "-dark" : "-light") : ""}.svg`
             return (
-              <div key={item.name} className="flex">
+              <div key={item.key} className="flex">
                 <Image
-                  src={item.icon}
-                  alt={item.name}
+                  src={url}
+                  alt={item.title}
                   width={48}
                   height={48}
                   quality={100}
@@ -26,7 +31,7 @@ export const TechStack: React.FC = () => {
                   variant={"link"}
                   className="ml-2 hidden p-0 sm:inline-block"
                 >
-                  {item.name}
+                  {item.title}
                 </Button>
               </div>
             )
