39ff2d9ededd7e9cd1d049a95c2afd2d6aff59ea
UdayKiran <uday@hexacluster.ai>
2025-08-16 19:21:33 +0530

    shiki code component for both light & dark themes

--- src/app/(root)/test/page.tsx
+++ src/app/(root)/test/page.tsx
@@ -1,3 +1,13 @@
-export default function page() {
+import { Code } from "@/components/code/code"
+import { ContainerWrapper } from "@/components/site/container"
+
+const code = `export default function page() {
   return <>Test Page</>
+}`
+export default function page() {
+  return (
+    <ContainerWrapper>
+      <Code code={code} />
+    </ContainerWrapper>
+  )
 }
--- src/components/code/code.tsx
+++ src/components/code/code.tsx
@@ -17,17 +17,19 @@ export async function Code({
     theme,
   })
 
+  const htmlLight = await codeToHtml(code, { lang, theme: "github-light" })
+  const htmlDark = await codeToHtml(code, { lang, theme: "github-dark" })
+
   return (
-    <div className="">
-      {/* <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pr-4 pl-2 text-sm">
-          <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-neutral-700 bg-neutral-800 px-4 py-2">
-            {"filename.tsx"}
-          </span>
-        </div> */}
+    <div className="relative">
       <div
-        className="text-sm [&_code]:block [&_code]:w-fit [&_code]:min-w-full [&>pre]:overflow-x-auto [&>pre]:!bg-neutral-900 [&>pre]:py-3 [&>pre]:pr-5 [&>pre]:pl-4 [&>pre]:leading-snug"
-        dangerouslySetInnerHTML={{ __html: html }}
-      ></div>
+        className="overflow-hidden rounded-md text-sm dark:hidden [&_code]:block [&_code]:w-fit [&_code]:min-w-full [&>pre]:overflow-x-auto [&>pre]:!bg-neutral-50 [&>pre]:py-3 [&>pre]:pr-5 [&>pre]:pl-4 [&>pre]:leading-snug"
+        dangerouslySetInnerHTML={{ __html: htmlLight }}
+      />
+      <div
+        className="hidden overflow-hidden rounded-md text-sm dark:block [&_code]:block [&_code]:w-fit [&_code]:min-w-full [&>pre]:overflow-x-auto [&>pre]:!bg-neutral-900 [&>pre]:py-3 [&>pre]:pr-5 [&>pre]:pl-4 [&>pre]:leading-snug"
+        dangerouslySetInnerHTML={{ __html: htmlDark }}
+      />
     </div>
   )
 }
