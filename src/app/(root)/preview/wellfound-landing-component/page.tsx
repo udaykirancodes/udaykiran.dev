a8de67f50f76e77d9c1a23d95ebd632d4f5e12ef
UdayKiran <uday@hexacluster.ai>
2025-08-26 10:31:43 +0530

    add preview for full screen

---
+++ src/app/(root)/preview/wellfound-landing-component/page.tsx
@@ -0,0 +1,9 @@
+import { WellFoundLandingAnimation } from "@/core/wellfound-landing-animation"
+
+export default function page() {
+  return (
+    <div className="h-[80vh] w-screen">
+      <WellFoundLandingAnimation />
+    </div>
+  )
+}
